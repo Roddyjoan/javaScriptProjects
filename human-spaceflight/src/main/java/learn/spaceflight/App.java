f1b54979fbae15f072e30f9a24c1a732c2f7581d
Roddy <rratsirarson@dev-10.com>
2023-02-07 14:58:12 -0800

    human spaceflight, a few starting script changes

--- human-spaceflight/src/main/java/learn/spaceflight/App.java
+++ human-spaceflight/src/main/java/learn/spaceflight/App.java
@@ -1,8 +1,14 @@
 package learn.spaceflight;
 
+import learn.spaceflight.personnel.Astronaut;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     public static void main(String[] args) {
-        // Spring code here...
+        ApplicationContext container = new ClassPathXmlApplicationContext("dependency-configuration.xml");
+        Astronaut astronaut = container.getBean(Astronaut.class);
+        System.out.println(astronaut);
     }
 }
